d0f80fce8bbad741873c0f647e52ca7da5b30e7e
Nishi-Shin <55019726+Nishi-Shin@users.noreply.github.com>
2020-10-08 08:57:12 +0900

    post_create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
@@ -20,6 +21,18 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    post = Posr.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
