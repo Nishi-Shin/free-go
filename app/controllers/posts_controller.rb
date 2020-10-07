ba523e17fdeca7ddfc6e8be80df0b7596a78f072
Nishi-Shin <55019726+Nishi-Shin@users.noreply.github.com>
2020-10-07 22:28:23 +0900

    create_user

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '956e7874c5492e3b88460d8ff5f92f9d02e3426dc20c87c41f02713268fdec84ff4825db3009f2f9b5f882a547b7006f6698835f721f6b6498a6a0eeb68ab8ec'
