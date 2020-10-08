30b1be0658ed0bd7379bde3f8099e5ab774ee9f9
Nishi-Shin <55019726+Nishi-Shin@users.noreply.github.com>
2020-10-08 10:19:14 +0900

    user_edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user! , except: [:index]
+
   def index
     @posts = Post.all
   end
@@ -15,18 +17,27 @@ class PostsController < ApplicationController
   def create 
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post), notice: '投稿に成功しました！'
+    else
+      render :new
+    end
   end
 
   def edit
     @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to posts_path, alert: 'アクセス制限です！'
+    end
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: '更新に成功しました！'
+    else
+      render :edit
+    end
   end
 
   def destroy
