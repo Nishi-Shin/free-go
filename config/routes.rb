a6982c51053fdaaf57b7ebb2d9be4029f257f52d
Nishi-Shin <55019726+Nishi-Shin@users.noreply.github.com>
2020-10-19 14:14:46 +0900

    sign_up_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
   sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
+  passwords:     'users/passwords'
 }
 
   devise_scope :user do
